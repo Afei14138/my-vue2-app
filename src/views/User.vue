1f2cfc3888c677702593a195a2bb4e339bd80fbc
wpf <625592378@qq.com>
2024-03-18 11:21:02 +0800

    引入弹窗

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,12 +1,20 @@
 <template>
-    <h1>我是 User</h1>
+    <div class="manage">
+        <el-dialog title="提示" :visible.sync="dialogVisible" width="30%" :before-close="handleClose">
+            <span>这是一段信息</span>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="dialogVisible = false">取 消</el-button>
+                <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+            </span>
+        </el-dialog>
+    </div>
 </template>
 
 
 <script>
-export default{
-    data(){
-        return{}
+export default {
+    data() {
+        return {}
     }
 }
 
