bca2610ce603e0f3a5540f377ffa9cc9b0c2745b
afei <625592378@qq.com>
2024-03-25 21:58:36 +0800

    添加表单校验功能

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,32 +1,34 @@
 <template>
     <div class="manage">
-        <el-dialog title="提示" :visible.sync="dialogVisible" width="30%">
+        <el-dialog title="提示" 
+        :visible.sync="dialogVisible" width="30%"
+        :before-close="handleClose">
             <!-- 用户的表单信息 -->
-            <el-form ref="form" :inline="true" :model="form" label-width="80px">
-                <el-form-item label="姓名">
+            <el-form ref="form" :rules="rules":inline="true" :model="form" label-width="80px">
+                <el-form-item label="姓名" prop="name">
                     <el-input placeholder="请输入姓名" v-model="form.name"></el-input>
                 </el-form-item>
-                <el-form-item label="年龄">
+                <el-form-item label="年龄" prop="age">
                     <el-input placeholder="请输入年龄" v-model="form.age"></el-input>
                 </el-form-item>
-                <el-form-item label="性别">
+                <el-form-item label="性别" prop="sex">
                     <el-select v-model="form.sex" placeholder="请选择">
                         <el-option label="男" value="1"></el-option>
                         <el-option label="女" value="0"></el-option>
                     </el-select>
                 </el-form-item>
-                <el-form-item label="出生日期">
+                <el-form-item label="出生日期" prop="birth">
                     <el-date-picker v-model="form.birth" type="date" placeholder="选择日期">
                     </el-date-picker>
                 </el-form-item>
-                <el-form-item label="地址">
+                <el-form-item label="地址" prop="addr">
                     <el-input placeholder="请输入地址" v-model="form.addr"></el-input>
                 </el-form-item>
             </el-form>
 
             <span slot="footer" class="dialog-footer">
-                <el-button @click="dialogVisible = false">取 消</el-button>
-                <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+                <el-button @click="cancel">取 消</el-button>
+                <el-button type="primary" @click="submit">确 定</el-button>
             </span>
         </el-dialog>
         <div class="manage-header">
@@ -49,7 +51,47 @@ export default {
                 sex: '',
                 birth: '',
                 addr: ''
+            },
+            rules:{
+                name:[
+                    { required: true, message: '请输入姓名'}
+                ],
+                age:[
+                    { required: true, message: '请输入年龄'}
+                ],
+                sex:[
+                    { required: true, message: '请选择性别'}
+                ],
+                birth:[
+                    { required: true, message: '请选择出生日期'}
+                ],
+                addr:[
+                    { required: true, message: '请输入地址'}
+                ]
             }
+        }
+    },
+    methods: {
+        submit(){
+            this.$refs.form.validate((valid)=>{
+                if(valid){
+                    // 说明校验通过才会进行后续的数据处理
+                    console.log(this.form)
+                    // 清空弹窗的数据
+                    this.handleClose()
+                    // 关闭弹窗
+                    this.dialogVisible = false
+                }
+            })
+
+        },
+        handleClose(){
+            // 弹窗关闭之前，清空表单数据
+            this.$refs.form.resetFields()
+            this.dialogVisible = false
+        },
+        cancel(){
+            this.handleClose()
         }
     }
 }
